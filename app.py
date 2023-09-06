8aff40f52200d2b037427f5ddbbe6ea2506c870a
SiranjeevM <139358854+SiranjeevM@users.noreply.github.com>
2023-09-06 11:41:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-from tensorflow.keras.preprocessing import image_
+from tensorflow.keras.preprocessing import image
 from tensorflow.keras.models import load_model
 import tensorflow_hub as hub
 from PIL.image import Image
